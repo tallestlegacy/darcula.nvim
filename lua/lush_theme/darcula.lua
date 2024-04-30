c59d332d760ada320b286170f1f24b62be3e2581
TallestLegacy <tallestlegacy@gmail.com>
2024-04-30 23:30:32 +0300

    basic treesitter support (WIP)

--- lua/lush_theme/darcula.lua
+++ lua/lush_theme/darcula.lua
@@ -1,3 +1,16 @@
+-- Built with,
+--
+--        ,gggg,
+--       d8" "8I                         ,dPYb,
+--       88  ,dP                         IP'`Yb
+--    8888888P"                          I8  8I
+--       88                              I8  8'
+--       88        gg      gg    ,g,     I8 dPgg,
+--  ,aa,_88        I8      8I   ,8'8,    I8dP" "8I
+-- dP" "88P        I8,    ,8I  ,8'  Yb   I8P    I8
+-- Yb,_,d88b,,_   ,d8b,  ,d8b,,8'_   8) ,d8     I8,
+--  "Y8P"  "Y888888P'"Y88P"`Y8P' "YY8P8P88P     `Y8
+
 local lush = require("lush")
 local hsl = lush.hsl
 
@@ -70,10 +83,10 @@ local theme = lush(function(injected_functions)
     MatchParen { bg = M.grey03 },
 
     -- gutter
-    LineNr { fg = M.fg, bg = M.purple },
+    LineNr { fg = M.grey02 },
     LineNrAbove { fg = M.fg.da(50) },
     LineNrBelow { fg = M.fg.da(50) },
-    CursorLineNr { fg = M.fg },
+    CursorLineNr { fg = M.fg, gui = bold },
 
     -- highlight
     Search { fg = M.white, bg = M.blue },
@@ -84,7 +97,7 @@ local theme = lush(function(injected_functions)
     DiffAdd { fg = M.darkgreen.da(20) },
     DiffAdded { DiffAdd },
     DiffChange { fg = M.yellow.da(20) },
-    DiffDelete { fg = M.red },
+    DiffDelete { fg = M.error },
     DiffRemoved { DiffDelete },
     DiffText { DiffChange, gui = underline },
 
@@ -133,22 +146,22 @@ local theme = lush(function(injected_functions)
     Conditional { Statement },
     Repeat { Statement },
     Operator { fg = M.fg },
-    Exception { fg = M.red },
+    Exception { fg = M.error },
 
-    PreProc { fg = M.orange }, --  generic Preprocessor
-    Include { PreProc },       -- preprocessor #include
-    Define { PreProc },        -- preprocessor #define
-    Macro { PreProc },         -- same as Define
-    PreCondit { PreProc },     -- preprocessor #if, #else, #endif, etc.
+    PreProc { fg = M.green }, --  generic Preprocessor
+    Include { PreProc },      -- preprocessor #include
+    Define { PreProc },       -- preprocessor #define
+    Macro { PreProc },        -- same as Define
+    PreCondit { PreProc },    -- preprocessor #if, #else, #endif, etc.
 
-    Type { fg = M.fg },
+    Type { fg = M.yellow },
     Typedef { Type },
     StorageClass { fg = M.purple }, -- static, register, volatile, etc.
     Structure { StorageClass },     -- struct, union, enum, etc.
 
     Special { fg = M.orange },      -- (preferred) any special symbol
     SpecialChar { Special },        -- special character in a constant
-    Tag { fg = M.yellow },          -- you can use CTRL-] on this
+    Tag { fg = M.orange },          -- you can use CTRL-] on this
     Delimiter { Special },          -- character that needs attention
     SpecialComment { Special },     -- special things inside a comment
     Debug { Special },              -- debugging statements
@@ -161,6 +174,68 @@ local theme = lush(function(injected_functions)
     Error { fg = M.error },            --  any erroneous construct
     Todo { fg = M.green, gui = bold }, --  anything that needs extra attention
 
+
+    -- ╭─────────────────────────────────────────────────────────╮
+    -- │                     treesitter                          │
+    -- ╰─────────────────────────────────────────────────────────╯
+
+    sym "@constant" { Constant },
+    sym "@constant.builtin" { Constant, gui = italic }, -- constant that are built in the language: `nil` in Lua.
+    sym "@constant.macro" { Constant, gui = bold },     -- constants that are defined by macros: `NULL` in C.
+    sym "@number" { Number },
+    sym "@float" { Float },
+    sym "@boolean" { Boolean },
+    sym "@character" { Character },
+    sym "@string" { String },
+    sym "@string.regex" { Character },
+    sym "@string.escape" { Character },           -- escape characters within a string
+    sym "@symbol" { fg = M.green, gui = italic }, -- For identifiers referring to symbols or atoms.
+
+
+    sym "@field" { fg = M.purple },
+    sym "@property" { fg = M.purple },
+    sym "@parameter" { fg = M.fg },
+    sym "@parameter.reference" { fg = M.fg },
+    sym "@variable" { fg = M.fg },                      -- Any variable name that does not have another highlight
+    sym "@variable.builtin" { Constant, gui = italic }, -- Variable names that are defined by the languages like `this` or `self`.
+
+
+    sym "@function" { Function },
+    sym "@function.builtin" { Function },
+    sym "@function.macro" { Function }, -- macro defined functions: each `macro_rules` in Rust
+    sym "@method" { Function },
+    sym "@constructor" { fg = M.fg },   -- For constructor: `{}` in Lua and Java constructors.
+    sym "@keyword.function" { Keyword },
+
+    sym "@keyword" { Keyword },
+    sym "@conditional" { Conditional },
+    sym "@repeat" { Repeat },
+    sym "@label" { Label },
+    sym "@operator" { Operator },
+    sym "@exception" { Exception },
+
+    sym "@namespace" { PreProc },  -- identifiers referring to modules and namespaces.
+    sym "@annotation" { PreProc }, -- C++/Dart attributes annotations that can be attached to the code to denote some kind of meta information
+    sym "@attribute" { PreProc },  -- Unstable
+    sym "@include" { PreProc },    -- includes: `#include` in C `use` or `extern crate` in Rust or `require` in Lua.
+
+    sym "@type" { Type },
+    sym "@type.builtin" { Type, gui = italic },
+
+    sym "@comment" { Comment },
+    sym "@punctuation.bracket" { fg = M.fg },   -- brackets and parens.
+    sym "@punctuation.delimiter" { Delimiter }, -- delimiters ie: `.`
+    sym "@punctuation.special" { Delimiter },   -- special punctutation that does not fall in the catagories before.
+    sym "@tag" { Tag },                         -- Tags like html tag names.da
+    sym "@tag.delimiter" { fg = M.white },      -- Tag delimiter like < > /
+    sym "@text" { fg = M.fg },
+    sym "@text.emphasis" { fg = M.fg, gui = italic },
+    sym "@text.literal" { String }, -- Literal text
+    sym "@text.strike" { Comment, gui = underline },
+    sym "@text.strong" { fg = M.fg, gui = bold },
+    sym "@text.title" { fg = M.orange },            -- Text that is part of a title
+    sym "@text.underline" { fg = M.fg, gui = underline },
+    sym "@text.uri" { fg = M.green, gui = italic }, -- Any URI like a link or email
   }
 end)
 

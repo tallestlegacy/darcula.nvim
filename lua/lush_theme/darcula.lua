79043c2666fc82015a8c6489fbac9c6e2c9e37ff
TallestLegacy <tallestlegacy@gmail.com>
2024-04-30 20:31:22 +0300

    basic syntax highlights (builtin)

--- lua/lush_theme/darcula.lua
+++ lua/lush_theme/darcula.lua
@@ -5,15 +5,15 @@ local hsl = lush.hsl
 local M = {}
 
 M.white = hsl(0, 0, 80)
-M.yellow = hsl(50, 83, 73)
-M.orange = hsl(15, 78, 52)
-M.darkred = hsl(15, 74, 40)
-M.purple = hsl(270, 23, 63)
-M.green = hsl(140, 100, 50)
+M.yellow = hsl(51, 76, 72)
+M.orange = hsl(27, 61, 50)
+M.darkred = hsl(17, 61, 40)
+M.purple = hsl(279, 23, 56)
+M.green = hsl(120, 61, 50)
 M.grey = hsl(0, 0, 50)
-M.darkgreen = hsl(115, 50, 43)
+M.darkgreen = hsl(108, 28, 46)
 M.lightblue = hsl(210, 63, 63)
-M.blue = hsl(210, 78, 33)
+M.blue = hsl(199, 69, 20)
 -- Base colors
 M.grey00 = hsl(0, 0, 9)  -- default background
 M.grey01 = hsl(0, 0, 16) -- lighter background (used for status bars, line number and folding marks)
@@ -30,7 +30,7 @@ M.fg = hsl(210, 7, 82)
 M.overbg = M.grey02
 M.pop = M.grey03
 -- Text
-M.comment = M.grey03
+M.comment = M.grey
 M.error = M.red
 M.warn = M.orange
 M.info = M.grey05
@@ -115,7 +115,51 @@ local theme = lush(function(injected_functions)
 
     -- syntax highlighting
 
+    Comment { fg = M.comment, gui = italic },
 
+    Constant { fg = M.lightblue },
+    Number { Constant },
+    Float { Number },
+    Boolean { Constant },
+    Character { fg = M.white },
+    String { fg = M.darkgreen },
+
+    Identifier { fg = M.fg },
+    Function { fg = M.yellow },
+
+    Statement { fg = M.orange }, -- (preferred) any statement
+    Label { fg = M.purple },     -- case, default, etc.
+    Keyword { Statement },       -- any other keyword
+    Conditional { Statement },
+    Repeat { Statement },
+    Operator { fg = M.fg },
+    Exception { fg = M.red },
+
+    PreProc { fg = M.orange }, --  generic Preprocessor
+    Include { PreProc },       -- preprocessor #include
+    Define { PreProc },        -- preprocessor #define
+    Macro { PreProc },         -- same as Define
+    PreCondit { PreProc },     -- preprocessor #if, #else, #endif, etc.
+
+    Type { fg = M.fg },
+    Typedef { Type },
+    StorageClass { fg = M.purple }, -- static, register, volatile, etc.
+    Structure { StorageClass },     -- struct, union, enum, etc.
+
+    Special { fg = M.orange },      -- (preferred) any special symbol
+    SpecialChar { Special },        -- special character in a constant
+    Tag { fg = M.yellow },          -- you can use CTRL-] on this
+    Delimiter { Special },          -- character that needs attention
+    SpecialComment { Special },     -- special things inside a comment
+    Debug { Special },              -- debugging statements
+
+
+    Underlined { gui = underline },
+    Bold { gui = bold },
+    Italic { gui = italic },
+    Ignore { fg = M.comment },         --  left blank, hidden  |hl-Ignore|
+    Error { fg = M.error },            --  any erroneous construct
+    Todo { fg = M.green, gui = bold }, --  anything that needs extra attention
 
   }
 end)

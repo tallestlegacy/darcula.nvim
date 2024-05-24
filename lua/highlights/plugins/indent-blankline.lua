b185cd6c1583ee94cac53a151ea23004a220fa58
TallestLegacy <tallestlegacy@gmail.com>
2024-05-24 15:14:51 +0300

    simple ibl updates

--- lua/highlights/plugins/indent-blankline.lua
+++ lua/highlights/plugins/indent-blankline.lua
@@ -1,7 +1,8 @@
 return {
   apply = function(palette, scheme)
     local M = {
-      IblScope = { fg = palette.layers.light[4] },
+      IblScope = { fg = palette.layers.light[6] },
+      IblIndent = { fg = palette.layers.light[1] },
     }
 
     return require("utils.helpers").mergeTables(scheme, M)
